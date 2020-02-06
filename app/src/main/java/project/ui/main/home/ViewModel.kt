906657c7c33b4ad4c5f3fa4b1f4f1c8575d4cadb
Sajjadahmadish <sajjad.ahmadi.sh@gmail.com>
2020-02-06 12:51:01 -0500

    home slider change

--- app/src/main/java/project/ui/main/home/ViewModel.kt
+++ app/src/main/java/project/ui/main/home/ViewModel.kt
@@ -9,8 +9,8 @@ class HomeViewModel(dataManager: DataManager, schedulerProvider: SchedulerProvid
 
     //
     val list = listOf(
-        "https://safirancp.faranegar.com/uploads/d8be0164-7b62-4e7c-9f6f-98d3c39acd3f//06.jpg",
-        "https://bisanseir.com/dist/admin/images/main_slide/org/main_slide_0.jpg",
+        "http://s7.picofile.com/file/8387312218/kish_offer.png",
+        "http://s7.picofile.com/file/8387312142/wrold_offer.png",
         "https://www.salehantravel.com/uploadfiles/05480cb180694ee28162ad4374b825d5.jpg",
         "https://www.salehantravel.com/uploadfiles/9ba8e256f2be4cb193e0a04cec7255e1.jpg"
     )
