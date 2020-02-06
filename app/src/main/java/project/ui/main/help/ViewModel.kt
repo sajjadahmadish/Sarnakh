a19bf65bce244eb91f6414d1a6b2759f11289799
Sajjadahmadish <sajjad.ahmadi.sh@gmail.com>
2020-02-06 18:55:28 -0500

    bestFragment ok

--- app/src/main/java/project/ui/main/help/ViewModel.kt
+++ app/src/main/java/project/ui/main/help/ViewModel.kt
@@ -1,8 +1,6 @@
 package project.ui.main.help
 
 import androidx.lifecycle.SavedStateHandle
-import com.squareup.inject.assisted.Assisted
-import com.squareup.inject.assisted.AssistedInject
 import project.data.DataManager
 import project.ui.base.BaseViewModel
 import project.utils.rx.SchedulerProvider
--- app/src/main/java/project/ui/scoreBoard/Navigator.kt
+++
@@ -1,4 +0,0 @@
-package project.ui.scoreBoard
-interface ScoreBoardNavigator {
-
-}
