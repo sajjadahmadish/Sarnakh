105a1a18d1fafe66a10120d2d6532cfb660c6e0b
Hossein <hosseinallahresani@gmail.com>
2020-02-06 19:37:49 +0330

    fix Bug viewModel mission & revolution of profile Activity

--- app/src/main/java/project/ui/mission/ViewModel.kt
+++ app/src/main/java/project/ui/mission/ViewModel.kt
@@ -7,7 +7,12 @@ import project.utils.rx.SchedulerProvider
 class MissionViewModel(dataManager: DataManager, schedulerProvider: SchedulerProvider)
     : BaseViewModel<MissionNavigator>(dataManager, schedulerProvider) {
 
-    //
+    val list = listOf(
+        "https://delta.ir/mag/wp-content/uploads/2019/05/%DA%AF%D9%84%D8%B3%D8%AA%D8%A7%D9%86.jpg",
+        "https://www.snapptrip.com/blog/wp-content/uploads/2018/12/%DA%A9%D8%A7%D8%AE-%DA%AF%D9%84%D8%B3%D8%AA%D8%A7%D9%86-650x406.jpg",
+        "https://images.kojaro.com/2016/6/12999f45-790d-4a70-8333-740ea8c30f35.jpg",
+        "https://mashrooh.com/images/content/tourism/1395/22/Golestan-Palace.jpg"
+    )
 
 }
 
