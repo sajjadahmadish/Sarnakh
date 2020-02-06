ae4eb3f48a21b89df30b8f68939b60eb285c8044
Sajjadahmadish <sajjad.ahmadi.sh@gmail.com>
2020-02-06 13:45:39 -0500

    slider home picture

--- app/src/main/java/project/ui/main/home/ViewModel.kt
+++ app/src/main/java/project/ui/main/home/ViewModel.kt
@@ -9,8 +9,8 @@ class HomeViewModel(dataManager: DataManager, schedulerProvider: SchedulerProvid
 
     //
     val list = listOf(
-        "http://s7.picofile.com/file/8387312218/kish_offer.png",
-        "http://s7.picofile.com/file/8387312142/wrold_offer.png",
+        "http://s6.picofile.com/file/8387319034/kish_offer_4x.png",
+        "http://s6.picofile.com/file/8387319076/wrold_offer_4x.png",
         "https://www.salehantravel.com/uploadfiles/05480cb180694ee28162ad4374b825d5.jpg",
         "https://www.salehantravel.com/uploadfiles/9ba8e256f2be4cb193e0a04cec7255e1.jpg"
     )
